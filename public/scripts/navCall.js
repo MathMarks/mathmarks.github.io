94c878caf8c904c1088bd590ae61277af7d4310b
Matheus Marques <matheusmarques.j@gmail.com>
2022-09-30 10:28:02 -0300

    v1.3.0 - Adicionado uma forma de incluir a nav em todos os lugares, como se fosse um partial do NodeJS, e atualizado todas as páginas com a nova navbar do site

--- public/scripts/navCall.js
+++ public/scripts/navCall.js
@@ -1,4 +1,23 @@
 
-var navBar = document.getElementById("navBar");
+load(document.getElementsByClassName('navCall')[0], './views/partials/navbar.html');
+loadAppend(document.getElementsByTagName('head')[0], './views/partials/headneeds.html');
 
-navBar.innerHTML = ;
+function load(target, url) {
+    var r = new XMLHttpRequest();
+    r.open("GET", url, true);
+    r.onreadystatechange = function () {
+        if (r.readyState != 4 || r.status != 200) return;
+        target.innerHTML = r.responseText;
+    };
+    r.send();
+}
+
+function loadAppend(target, url) {
+    var r = new XMLHttpRequest();
+    r.open("GET", url, true);
+    r.onreadystatechange = function () {
+        if (r.readyState != 4 || r.status != 200) return;
+        target.innerHTML += r.responseText;
+    };
+    r.send();
+}
