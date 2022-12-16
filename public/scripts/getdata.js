83c4088d30a2d2ec7cc3b9e157aa930f5dcaff88
Matheus Marques <matheusmarques.j@gmail.com>
2022-12-16 15:07:14 -0300

    adicionado o select para selecionar a cidade de interesse

--- public/scripts/getdata.js
+++ public/scripts/getdata.js
@@ -5,8 +5,8 @@ const url = "https://docs.google.com/spreadsheets/d/"
 const query1 = `/gviz/tq?`
 
 const endpoint1 = `${url}${sheetID}${query1}`;
-
-console.log(endpoint1);
+/* 
+console.log(endpoint1); */
 
 fetch(endpoint1)
 .then(response => response.text())
@@ -16,12 +16,127 @@ fetch(endpoint1)
     //console.log(temp);
     const json = JSON.parse(temp);
     //console.log(json);
-    const rows = json.table.rows;
+    const rows = json.table.rows;/* 
     console.log(rows);
-    const content = document.getElementsByClassName('my_items')[0];
-    console.log(rows['length']);
-    console.log(rows[0].c[0].v);
+    
+    console.log(rows['length']); */
+    //console.log(rows[0].c[0].v);
+    const cidadesWrap = document.getElementsByClassName('cidades')[0];
+    renderProducts(rows);
+
+    window.localStorage.setItem('PRODUTOS', JSON.stringify(rows));
+
+    cidadesWrap.addEventListener('click', function () { 
+
+        var localRows = JSON.parse(window.localStorage.getItem('PRODUTOS'));
+        arrSelectedRows = [];
+        var arrSelectedLabels = [];
+        //console.log(localRows);
+        for (let i = 0; i < cidadesWrap.getElementsByTagName('label').length; i++) {
+            
+            if(cidadesWrap.getElementsByTagName('input')[i].checked){
+
+                arrSelectedLabels.push(String(cidadesWrap.getElementsByTagName('label')[i].innerText).trim());
+
+            }
+            
+            
+        }
+
+        localRows.forEach((element,index)=> {
+            console.log(arrSelectedLabels.includes(String(element.c[7].v).trim()));
+            if(arrSelectedLabels.includes(String(element.c[7].v).trim())){
+                //console.log(element.c[7].v, index);
+                arrSelectedRows.push(element);
+            }
+        });
+
+        if(arrSelectedRows.length == 0){
+            renderProducts(localRows);
+        } else {
+
+            //console.log(arrSelectedRows);
+            renderProducts(arrSelectedRows);
+            arrSelectedRows = [];
+
+        }
+
+     });
+        
+
+    }
+
+);
+
+
+function imageUsableURL(url){
+
+    if(String(url).includes("drive.google")){
+
+        console.log("Imagem do Drive");
+        const imgID = String(url).split("d/")[1].split("/")[0]; //Get the image ID
+        const usableURL = `https://drive.google.com/uc?export=view&id=${imgID}`;
+
+        return usableURL;
+
+    } else {
+
+        const usableURL = String(url);
+
+        return usableURL;
+
+    }
+
+}
+
+function createIconsInfo(cidade, condi, article){
+
+    const divExterna = document.createElement('div');
+    const spanWrapCid = document.createElement('span');
+    const spanIconCid = document.createElement('span');
+    const spanTextoCid = document.createElement('span');
+    const iconCid = document.createElement('i');
+
+    const spanWrapCond = document.createElement('span');
+    const spaniconCond = document.createElement('span');
+    const spanTextoCond = document.createElement('span');
+    const iconCond = document.createElement('i');
+
+    divExterna.appendChild(spanWrapCid);
+    spanWrapCid.appendChild(spanIconCid);
+    spanWrapCid.appendChild(spanTextoCid);
+    spanIconCid.appendChild(iconCid);
+
+    divExterna.setAttribute('class', 'level is-flex mt-5 is-justify-content-space-between');
+    spanWrapCid.setAttribute('class','icon-text');
+    spanIconCid.setAttribute('class', 'icon');
+    iconCid.setAttribute('class','fa-sharp fa-solid fa-location-dot');
+    spanTextoCid.innerText = String(cidade);
+
+    spaniconCond.appendChild(iconCond);
+    spanWrapCond.appendChild(spaniconCond);
+    spanWrapCond.appendChild(spanTextoCond);
+    
+
+    divExterna.appendChild(spanWrapCond);
+
+    spanWrapCond.setAttribute('class','icon-text');
+    spaniconCond.setAttribute('class', 'icon');
+    iconCond.setAttribute('class','fa-solid fa-star');
+    iconCond.style.color = "gold";
+    spanTextoCond.innerText = String(condi);
+
+
+    article.appendChild(divExterna);
+
+}
+
+function renderProducts(rows){
+
     var count = 0;
+    const content = document.getElementsByClassName('my_items')[0];
+    content.innerHTML = "";
+
 
     for(let i = rows['length'] ; i > 0 ; i){
 
@@ -84,68 +199,5 @@ fetch(endpoint1)
         }
 
     }
-
-});
-
-
-function imageUsableURL(url){
-
-    if(String(url).includes("drive.google")){
-
-        console.log("Imagem do Drive");
-        const imgID = String(url).split("d/")[1].split("/")[0]; //Get the image ID
-        const usableURL = `https://drive.google.com/uc?export=view&id=${imgID}`;
-
-        return usableURL;
-
-    } else {
-
-        const usableURL = String(url);
-
-        return usableURL;
-
-    }
-
-}
-
-function createIconsInfo(cidade, condi, article){
-
-    const divExterna = document.createElement('div');
-    const spanWrapCid = document.createElement('span');
-    const spanIconCid = document.createElement('span');
-    const spanTextoCid = document.createElement('span');
-    const iconCid = document.createElement('i');
-
-    const spanWrapCond = document.createElement('span');
-    const spaniconCond = document.createElement('span');
-    const spanTextoCond = document.createElement('span');
-    const iconCond = document.createElement('i');
-
-    divExterna.appendChild(spanWrapCid);
-    spanWrapCid.appendChild(spanIconCid);
-    spanWrapCid.appendChild(spanTextoCid);
-    spanIconCid.appendChild(iconCid);
-
-    divExterna.setAttribute('class', 'level is-flex mt-5 is-justify-content-space-between');
-    spanWrapCid.setAttribute('class','icon-text');
-    spanIconCid.setAttribute('class', 'icon');
-    iconCid.setAttribute('class','fa-sharp fa-solid fa-location-dot');
-    spanTextoCid.innerText = String(cidade);
-
-    spaniconCond.appendChild(iconCond);
-    spanWrapCond.appendChild(spaniconCond);
-    spanWrapCond.appendChild(spanTextoCond);
-    
-
-    divExterna.appendChild(spanWrapCond);
-
-    spanWrapCond.setAttribute('class','icon-text');
-    spaniconCond.setAttribute('class', 'icon');
-    iconCond.setAttribute('class','fa-solid fa-star');
-    iconCond.style.color = "gold";
-    spanTextoCond.innerText = String(condi);
-
-
-    article.appendChild(divExterna);
 
 }
